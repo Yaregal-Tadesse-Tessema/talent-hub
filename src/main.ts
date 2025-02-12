af131888b0244dac9818b1bfaaca521895fccf24
Yaregal-Tadeese-Tessema <yaregal@garrilogistics.com>
2025-02-12 23:00:41 +0300

    swagger fix

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerCustomOptions, SwaggerModule } from '@nestjs/swagger';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
