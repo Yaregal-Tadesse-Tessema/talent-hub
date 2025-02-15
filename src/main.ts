d627da924e3063af454388671fdf3e21d1325a90
Yaregal-Tadeese-Tessema <yaregal@garrilogistics.com>
2025-02-16 01:04:41 +0300

    filter

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerCustomOptions, SwaggerModule } from '@nestjs/swagger';
+import { GlobalExceptionFilter } from './libs/Common/filters/error-handling';
+import { EventEmitter2 } from '@nestjs/event-emitter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalFilters(new GlobalExceptionFilter(app.get(EventEmitter2)));
   const customOptions: SwaggerCustomOptions = {
     swaggerOptions: {
       persistAuthorization: false,
