4ad5f864ec52337bc6a22701f6ef17f7d883cc06
Yaregal-Tadeese-Tessema <yaregal@garrilogistics.com>
2025-05-05 16:05:58 +0300

    what is going on

--- src/modules/auth/refreshToken.strategy.ts
+++ src/modules/auth/refreshToken.strategy.ts
@@ -1,18 +1,18 @@
 /* eslint-disable prettier/prettier */
+import * as dotenv from 'dotenv';
+dotenv.config({ path: '.env' });
+import * as process from 'node:process';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { Request } from 'express';
 import { Injectable } from '@nestjs/common';
-import * as dotenv from 'dotenv';
-dotenv.config({ path: '.env' });
-import * as process from 'node:process';
 @Injectable()
 export class RefreshTokenStrategy extends PassportStrategy(
   Strategy,
   'jwt-refresh',
 ) {
   constructor() {
-    const secret = process.env.TOKEN_SECRET_KEY;
+    const secret = process.env.REFRESH_TOKEN_SECRET_KEY;
     console.log(secret);
     if (!secret) {
       throw new Error('TOKEN_SECRET_KEY is not defined');
