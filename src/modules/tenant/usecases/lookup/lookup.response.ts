837431b0bb80bfa886dd72d461ea2b1ef37ceff1
Yaregal-Tadesse <yayasoles@gmail.com>
2025-05-26 14:45:24 +0300

    added response

--- src/modules/tenant/usecases/lookup/lookup.response.ts
+++ src/modules/tenant/usecases/lookup/lookup.response.ts
@@ -1,61 +1,71 @@
 /* eslint-disable prettier/prettier */
-import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty } from "class-validator";
-import { LookupEntity } from "../../persistencies/lookup.entity";
-import { AccountStatusEnums } from "src/modules/auth/constants";
-import { CommonResponses } from "src/libs/Common/common.responses";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
+import { LookupEntity } from '../../persistencies/lookup.entity';
+import { AccountStatusEnums } from 'src/modules/auth/constants';
+import { CommonResponses } from 'src/libs/Common/common.responses';
+import { UserType } from '../../constants';
+import { FileDto } from 'src/libs/Common/dtos/file.dto';
 
 export class LookupResponse extends CommonResponses {
-    @ApiProperty({
-      example: 'uuid',
-    })
-    @IsNotEmpty()
-      id?: string;
-      @ApiProperty()
-      firstName?: string;
-      @ApiProperty()
-      middleName?: string;
-      @ApiProperty()
-      fullName?: string;
-      @ApiProperty()
-      lastName?: string;
-      @ApiProperty()
-      password: string;
-      @ApiProperty()
-      email: string;
-      @ApiProperty()
-      phoneNumber: string;
-      @ApiProperty()
-      status: AccountStatusEnums;
-      currentUser?: any;
+  @ApiProperty({
+    example: 'uuid',
+  })
+  @IsNotEmpty()
+  id?: string;
+  @ApiProperty()
+  firstName?: string;
+  @ApiProperty()
+  middleName?: string;
+  @ApiProperty()
+  fullName?: string;
+  @ApiProperty()
+  lastName?: string;
+  @ApiProperty()
+  password: string;
+  @ApiProperty()
+  email: string;
+  @ApiProperty()
+  phoneNumber: string;
+  @ApiProperty()
+  status: AccountStatusEnums;
+  @ApiProperty()
+  userType: UserType;
+  @ApiProperty()
+  profileImage: FileDto;
+  @ApiProperty()
+  address: any;
+  currentUser?: any;
 
-      @ApiProperty()
-      jobTitle?: string;
-      @ApiProperty()
-      startDate?: Date;
-      @ApiProperty()
-      tenantId?: string;
-      @ApiProperty()
-      tenantName?: string;
+  @ApiProperty()
+  jobTitle?: string;
+  @ApiProperty()
+  startDate?: Date;
+  @ApiProperty()
+  tenantId?: string;
+  @ApiProperty()
+  tenantName?: string;
 
-     
-    static toResponse(entity: LookupEntity): LookupResponse {
-      const response = new LookupResponse();
-      response.id = entity?.id;
-      response.fullName = `${entity?.firstName} ${entity?.middleName} ${entity?.lastName}`;
-      response.firstName = entity.firstName;
-      response.middleName = entity.middleName;
-      response.lastName = entity.lastName;
-      response.email = entity.email;
-      response.phoneNumber = entity.phoneNumber;
-      response.status = entity?.status;
-      
-      response.createdBy = entity.createdBy;
-      response.updatedBy = entity.updatedBy;
-      response.deletedBy = entity.deletedBy;
-      response.createdAt = entity.createdAt;
-      response.updatedAt = entity.updatedAt;
-      response.deletedAt = entity.deletedAt;
-      return response;
-    }
+  static toResponse(entity: LookupEntity): LookupResponse {
+    const response = new LookupResponse();
+    response.id = entity?.id;
+    response.fullName = `${entity?.firstName} ${entity?.middleName} ${entity?.lastName}`;
+    response.firstName = entity.firstName;
+    response.middleName = entity.middleName;
+    response.lastName = entity.lastName;
+    response.email = entity.email;
+    response.phoneNumber = entity.phoneNumber;
+    response.status = entity?.status;
+    response.userType = entity?.userType;
+    response.profileImage = entity?.profileImage;
+    response.address = entity?.address;
+
+    response.createdBy = entity.createdBy;
+    response.updatedBy = entity.updatedBy;
+    response.deletedBy = entity.deletedBy;
+    response.createdAt = entity.createdAt;
+    response.updatedAt = entity.updatedAt;
+    response.deletedAt = entity.deletedAt;
+    return response;
   }
+}
