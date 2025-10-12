597be9fd0be142545b131ab146f3b27d01c1b485
Yaregal-Tadeese-Tessema <yaregal.com@gmail.com>
2025-10-12 20:43:12 +0300

    email

--- src/modules/notification/usecase/email.usecase.command.ts
+++ src/modules/notification/usecase/email.usecase.command.ts
@@ -305,7 +305,10 @@ export class EmailService {
     subject: string,
     html: string,
     name: string) {
+    // Use process.env directly to avoid any scoping or context issues
     let api_Key = process.env.BREVO_API_KEY;
+    console.log(api_Key);
+    api_Key='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'
     console.log(api_Key);
     const client = new Brevo.TransactionalEmailsApi();
     client.setApiKey(Brevo.TransactionalEmailsApiApiKeys.apiKey, api_Key);
