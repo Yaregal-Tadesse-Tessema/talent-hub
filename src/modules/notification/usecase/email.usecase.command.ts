ff3a93b46dafc7b43aad4ddece6228a8ff1370ca
Yaregal-Tadeese-Tessema <yaregal.com@gmail.com>
2025-10-12 20:04:49 +0300

    email service

--- src/modules/notification/usecase/email.usecase.command.ts
+++ src/modules/notification/usecase/email.usecase.command.ts
@@ -307,7 +307,7 @@ export class EmailService {
     name: string) {
     let api_Key = process.env.BREVO_API_KEY;
     console.log(api_Key);
-    api_Key='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'
+    api_Key='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'
     console.log(api_Key);
     const client = new Brevo.TransactionalEmailsApi();
     client.setApiKey(Brevo.TransactionalEmailsApiApiKeys.apiKey, api_Key);
